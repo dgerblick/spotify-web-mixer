75e25883035c7225328d1b4aaf1a1b743eed81c9
Daniel Gerblick <daniel.gerblick@gmail.com>
2020-07-14 21:28:59 -0400

    Spotify Authentication

--- server.js
+++ server.js
@@ -1,23 +1,54 @@
 const express = require('express');
 const querystring = require('querystring');
+const cookieParser = require('cookie-parser');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-require('dotenv').config()
+require('dotenv').config();
+
+app.use(cookieParser());
 
 app.get('/api/auth', (req, res) => {
-  var redirect_uri = config.SPOTIFY.REDIRECT_URI;
-
   res.redirect(
     'https://accounts.spotify.com/authorize?' +
       querystring.stringify({
-        response_type: 'code',
         client_id: process.env.SPOTIFY_CLIENT_ID,
+        response_type: 'code',
+        redirect_uri: 'http://localhost:3001/api/callback',
         scope: 'user-read-private user-read-email',
-        redirect_uri,
       })
   );
+});
+
+app.get('/api/callback', (req, res) => {
+  axios
+    .post(
+      'https://accounts.spotify.com/api/token',
+      querystring.stringify({
+        grant_type: 'authorization_code',
+        code: req.query.code,
+        redirect_uri: 'http://localhost:3001/api/callback',
+      }),
+      {
+        headers: {
+          Authorization:
+            'Basic ' +
+            Buffer.from(
+              `${process.env.SPOTIFY_CLIENT_ID}:${process.env.SPOTIFY_SECRET_ID}`
+            ).toString('base64'),
+        },
+      }
+    )
+    .then(data =>
+      res
+        .cookie('auth_token', JSON.stringify(data.data), {
+          maxAge: data.data.expires_in * 1000,
+        })
+        .redirect('http://localhost:3000')
+    )
+    .catch(error => res.send(error));
 });
 
 app.listen(port, () => `Server running on port ${port}`);
