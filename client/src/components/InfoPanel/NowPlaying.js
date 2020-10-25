534456464595081d79ff4621c8b8d06f22fb726d
Daniel Gerblick <daniel.gerblick@gmail.com>
2020-10-25 00:54:27 -0400

    more controls

--- client/src/components/InfoPanel/NowPlaying.js
+++ client/src/components/InfoPanel/NowPlaying.js
@@ -6,14 +6,21 @@ import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
 import SkipNextIcon from '@material-ui/icons/SkipNext';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
+import ShuffleIcon from '@material-ui/icons/Shuffle';
+import RepeatIcon from '@material-ui/icons/Repeat';
+import RepeatOneIcon from '@material-ui/icons/RepeatOne';
 
 const NowPlaying = props => {
   const [playing, setPlaying] = useState();
 
   useEffect(() => {
+    axios
+      .get('https://api.spotify.com/v1/me/player')
+      .then(res => res.data)
+      .then(playback => setPlaying(playback));
     const interval = setInterval(() => {
       axios
-        .get('https://api.spotify.com/v1/me/player/currently-playing')
+        .get('https://api.spotify.com/v1/me/player')
         .then(res => res.data)
         .then(playback => setPlaying(playback));
     }, props.updateRate);
@@ -50,6 +57,15 @@ const NowPlaying = props => {
             </p>
           </div>
           <div className="controls">
+            <ShuffleIcon
+              className={playing.shuffle_state ? 'selected' : ''}
+              onClick={() =>
+                axios.put(
+                  'https://api.spotify.com/v1/me/player/shuffle?state=' +
+                    !playing.shuffle_state
+                )
+              }
+            />
             <SkipPreviousIcon
               onClick={() =>
                 axios.post('https://api.spotify.com/v1/me/player/previous')
@@ -73,6 +89,26 @@ const NowPlaying = props => {
                 axios.post('https://api.spotify.com/v1/me/player/next')
               }
             />
+            {playing.repeat_state === 'track' ? (
+              <RepeatOneIcon
+                className="selected"
+                onClick={() =>
+                  axios.put(
+                    'https://api.spotify.com/v1/me/player/repeat?state=off'
+                  )
+                }
+              />
+            ) : (
+              <RepeatIcon
+                className={playing.repeat_state === 'context' ? 'selected' : ''}
+                onClick={() =>
+                  axios.put(
+                    'https://api.spotify.com/v1/me/player/repeat?state=' +
+                      (playing.repeat_state === 'context' ? 'track' : 'context')
+                  )
+                }
+              />
+            )}
           </div>
         </SongDisplay>
       )}
@@ -81,7 +117,7 @@ const NowPlaying = props => {
 };
 
 NowPlaying.defaultProps = {
-  updateRate: 500,
+  updateRate: 1000,
 };
 
 export default NowPlaying;
